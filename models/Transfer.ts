b2120d2f912b69cee18026bbb0e3b7fcad63746d
Luigi Moraes <luigimoraes3@gmail.com>
2022-10-12 02:01:50 -0300

    Added Tranfer class for bank transfer operations

---
+++ models/Transfer.ts
@@ -0,0 +1,69 @@
+import Account from './Account';
+
+class Transfer {
+	private _accountOrigin: Account;
+	private _accountReceiver: Account;
+	private _money: number;
+	private _tansferStatus: string;
+
+	constructor(accOrigin: Account, accReceiver: Account){
+		this._accountOrigin = accOrigin;
+		this._accountReceiver = accReceiver;
+		this._money = 0.00;
+		this._transferStatus = "Not finished";
+	}
+
+	public getData(){
+		return {
+			acc_origin: this.accountOrigin.id,
+			acc_receiver: this.accountReceiver.id,
+			money: this.money,
+			transfer_status: this.transferStatus
+		};
+	}
+
+	public send(money: number): void {
+		this.accountOrigin.money -= money;
+		this.accountReceiver += money;
+
+		this.transferStatus = "Finished";
+	}
+
+	public destroy(): void {
+		this.transferStatus = "Cancelled";
+	}
+
+	private get accountOrigin(): Account {
+		return this._accountOrigin;
+	}
+
+	private set accountOrigin(newAccOrigin: Account){
+		this._accountOrigin = newAccOrigin;
+	}
+
+	private get accountReceiver(): Account {
+		return this._accountReceiver;
+	}
+
+	private set accountReceiver(newAccReceiver: Account){
+		this._accountReceiver = newAccReceiver;
+	}
+
+	private get money(): number {
+		return this._money;
+	}
+
+	private set money(newMoney: Account){
+		this._money = newMoney;
+	}
+
+	private get transferStatus(): string {
+		return this._transferStatus;
+	}
+
+	private set transferStatus(newTransferStatus: Account){
+		this._transferStatus = newTransferStatus;
+	}
+}
+
+export default Transfer;
